107140cae38792e9cf66a706eb905081eb944a6b
yemistring <beckyrain200@gmail.com>
2022-12-08 11:28:56 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main Entry point
+ *
+ * return:Always 0 (success)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
+
+
+}
